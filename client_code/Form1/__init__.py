f4e994fc1597a4d215cb647dea07edbc74cee737
d.fischer@tu-berlin.de <d.fischer@tu-berlin.de>
2024-03-06 13:48:27 +0000

    Created Form2; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from ..Form2 import Form2
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,16 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  def deselect_all_links(self):
+    """Reset all the roles on the navbar links."""
+    for link in self.link_1, self.link_2, self.link_3:
+      link.role = ''
+      
+
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Form2())
+    self.deselect_all_links()
+    self.link_1.role = 'selected'
