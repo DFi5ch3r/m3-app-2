302c0800f3e83e02ebb8ca328345697001409949
d.fischer@tu-berlin.de <d.fischer@tu-berlin.de>
2024-03-11 10:15:35 +0000

    Edited plots

--- client_code/plots/__init__.py
+++ client_code/plots/__init__.py
@@ -13,3 +13,16 @@ class plots(plotsTemplate):
   def button_plotPrep_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.fig1, self.fig2, self.fig3 = anvil.server.call('prepPlots')
+
+  def button_plot1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.plot_2.figure = self.fig1
+
+  def button_plot2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.plot_2.figure = self.fig2
+
+  def button_plot3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.plot_2.figure = self.fig3
+  
