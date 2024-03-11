3816ffb4d82feb2cc075dacad801f13cd118226a
d.fischer@tu-berlin.de <d.fischer@tu-berlin.de>
2024-03-11 10:12:33 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,8 +1,8 @@
 import anvil.server
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.express as px
+
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -18,19 +18,15 @@ import matplotlib.pyplot as plt
 @anvil.server.callable
 def prepPlots():
 
-  fig1 = plt.figure()
-  fig2 = plt.figure()
-  fig2 = plt.figure()
-  
   x = np.array(range(-50,50))
   y1 = x
   y2 = x*x
   y3 = x*x*x
   d = {'x':x, 'y1':y1, 'y2':y2, 'y3':y3,}
   df = pd.DataFrame(d)
-  sns.set_theme()
-  fig1 = sns.lineplot(data=df, x='x', y='y1')
-  fig2 = sns.lineplot(data=df, x='x', y='y2')
-  fig3 = sns.lineplot(data=df, x='x', y='y3')
+
+  fig1 = px.line(df, x='x', y='y1')
+  fig2 = px.line(df, x='x', y='y2')
+  fig3 = px.line(df, x='x', y='y3')
   
   return fig1, fig2, fig3
